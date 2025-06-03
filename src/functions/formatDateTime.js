20d14f742ec4acb4ae9f56d5d6e0f7db2c958104
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-06-03 15:45:59 -0400

    Campacion de datos para cita y paciente, formato de hora y fecha de cita.

---
+++ src/functions/formatDateTime.js
@@ -0,0 +1,14 @@
+export function mergeDateTime(rawDate, rawTime){
+    const date = new Date(rawDate)
+    const time = new Date(rawTime)
+
+    const year = date.getFullYear()
+    const month = date.getMonth()+1 <= 9 ? (`0${date.getMonth()+1}`):(date.getMonth()+1)
+    const day = date.getDate() <= 9 ? (`0${date.getDate()}`):(date.getDate())
+
+    const hour = time.getHours() <= 9 ? (`0${time.getHours()}`):(time.getHours())
+    const minutes = time.getMinutes() <=9 ? (`0${time.getMinutes()}`):(time.getMinutes())
+
+    const res = `${year}-${month}-${day} ${hour}:${minutes}:00`
+    return res
+}
--- src/pages/NewDate.jsx
+++ src/pages/NewDate.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from "react";
-import { Divider, Form, Input, Select, DatePicker, Space, Button } from 'antd'
+import { Divider, Form, Input, Select, DatePicker, Space, Button, TimePicker } from 'antd'
 import { verifyPatientExist } from '../client/client'
 import AppContext from "antd/es/app/context";
 import * as lists from "../context/lists"
+import { mergeDateTime } from "../functions/formatDateTime";
 
 const NewDate = () => {
 
@@ -15,9 +16,13 @@ const NewDate = () => {
     const [sex, setSex] = useState()
     const [birthDate, setBirthDate] = useState()
     const [date, setDate] = useState()
+    const [time, setTime] = useState()
     const [doctorId, setDoctorId] = useState()
     const [idType, setIdType] = useState()
     const [race, setRace] = useState()
+    const [emergencyRelation, setEmergencyRelation] = useState()
+    const [companionRelation, setCompanionRelation] = useState()
+    const [instructionGrade, setInstructionGrade] = useState()
 
     //Comunicacion back end
     const verifyPatient = async(e) => {
@@ -47,41 +52,43 @@ const NewDate = () => {
             const id = document.getElementById("idField").value
             const name = document.getElementById("nameField").value
             const lastname = document.getElementById("lastnameField").value
-            const sexField = sex
-            const birthDateField = birthDate
             const birthPlace = document.getElementById("birthPlaceField").value
             const phone = document.getElementById("phoneField").value
             const municipality = document.getElementById("phoneField").value
             const city = document.getElementById("cityField").value
             const address = document.getElementById("addressField").value
+            const religion = document.getElementById("religionField").value
+            const emergencyName = document.getElementById("emergencyNameField").value
+            const emergencyPhone = document.getElementById("emergencyPhoneField").value
+            const companionName = document.getElementById("companionNameField").value
+            const companionPhone = document.getElementById("companionPhoneField").value
 
             const data = {
                 name: name,
                 lastname: lastname,
                 identificationType: idType,
                 patientIdentification: id,
-                // patientCode: number,
                 phone: phone,
                 sex: sex,
-                // bloodType: number,
-                birthDate: birthDate,
+                birthDate: birthDate.$d,
                 birthPlace: birthPlace,
-                religion: string,
+                religion: religion,
                 race: race,
                 address: address,
                 addressMunicipality: municipality,
                 addressCity: city,
-                emergencyName: string,
-                emergencyPhone: string,
-                emergencyRelationship: string,
-                companionName: string,
-                companionPhone: string,
-                companionRelationship: string,
-                instructionGrade: number,
-                // ailments: number[],
-                // idStudent: string,
-                // idTeacher: string,
+                emergencyName: emergencyName,
+                emergencyPhone: emergencyPhone,
+                emergencyRelationship: emergencyRelation,
+                companionName: companionName,
+                companionPhone: companionPhone,
+                companionRelationship: companionRelation,
+                instructionGrade: instructionGrade,
+                date: mergeDateTime(date, time),
+                doctorId: doctorId
             }
+
+            console.log(data)
         }
     }
 
@@ -103,18 +110,43 @@ const NewDate = () => {
                             <Input id="lastnameField"/>
                         </Form.Item>
                         <Form.Item label="Tipo de identificacion:">
-                            <Select options={lists.identificationList.slice(0, 2)} style={{width: '150px'}}/>
+                            <Select
+                                options={lists.identificationList.slice(0, 2)}
+                                style={{width: '150px'}}
+                                onChange={e=>setIdType(e)}
+                            />
+                        </Form.Item>
+                        <Form.Item label="Fecha de nacimiento">
+                            <DatePicker
+                                style={{width: '150px'}}
+                                onChange={e=>setBirthDate(e)}    
+                            />
                         </Form.Item>
                     </Space>
                     <Space>
                         <Form.Item label="Sexo">
-                            <Select options={lists.sexList} style={{width: '150px'}}/>
+                            <Select
+                                options={lists.sexList}
+                                style={{width: '150px'}}
+                                onChange={e=>setSex(e)}    
+                            />
                         </Form.Item>
                         <Form.Item label="Raza:">
-                            <Select options={lists.raceList} style={{width: '150px'}}/>
+                            <Select
+                                options={lists.raceList}
+                                style={{width: '150px'}}
+                                onChange={e=>setRace(e)}    
+                            />
                         </Form.Item>
-                        <Form.Item label="Fecha de nacimiento">
-                            <DatePicker style={{width: '150px'}}/>
+                        <Form.Item label="Religion (opcional):">
+                            <Input id="religionField"/>
+                        </Form.Item>
+                        <Form.Item label="Grado de instruccion:">
+                            <Select
+                                style={{width: '150px'}}
+                                options={lists.instructionGradeList}
+                                onChange={e=>setInstructionGrade(e)}
+                            />
                         </Form.Item>
                     </Space>
                     <Space>
@@ -137,25 +169,33 @@ const NewDate = () => {
                     <Divider>Datos del contacto de emergencia</Divider>
                     <Space>
                         <Form.Item label="Contacto de emergencia">
-                            <Input/>
+                            <Input id="emergencyNameField"/>
                         </Form.Item>
                         <Form.Item label="Telefono">
-                            <Input/>
+                            <Input id="emergencyPhoneField"/>
                         </Form.Item>
                         <Form.Item label="Relacion">
-                            <Select options={lists.relationshipType} style={{width: '150px'}}/>
+                            <Select
+                                options={lists.relationshipType}
+                                style={{width: '150px'}}
+                                onChange={e=>setEmergencyRelation(e)}    
+                            />
                         </Form.Item>                        
                     </Space>
                     <Divider>Datos del acompanante habitual</Divider>
                     <Space>
                         <Form.Item label="Nombre (Opcional)">
-                            <Input/>
+                            <Input id="companionNameField"/>
                         </Form.Item>
                         <Form.Item label="Telefono (Opcional)">
-                            <Input/>
+                            <Input id="companionPhoneField"/>
                         </Form.Item>
                         <Form.Item label="Relacion (Opcional)">
-                            <Select options={lists.relationshipType} style={{width: '150px'}}/>
+                            <Select
+                                options={lists.relationshipType}
+                                style={{width: '150px'}}
+                                onChange={e=>setCompanionRelation(e)}    
+                            />
                         </Form.Item>                        
                     </Space>
 
@@ -171,17 +211,27 @@ const NewDate = () => {
                     
                     <Space>
                         <Form.Item label="Doctor:">
-                            <Select style={{width: '150px'}}/>
+                            <Select
+                                style={{width: '150px'}}
+                                onChange={e=>setDoctorId(e)}
+                            />
                         </Form.Item>
                         <Form.Item label="Fecha:">
-                            <DatePicker/>
+                            <DatePicker
+                                format="DD/MM/YYYY"
+                                onChange={(a, b)=>setDate(a.$d)}
+                            />
                         </Form.Item>
                         <Form.Item label="Hora:">
-                            <DatePicker/>
+                            <TimePicker
+                                onChange={(a, b)=>setTime(a.$d)}
+                                use12Hours
+                                format="hh:mm a"
+                            />
                         </Form.Item>
                     </Space>
 
-                    <Button variant="solid" color="primary" onClick={saveDate}>Registrar</Button>
+                    <Button htmlType="submit" variant="solid" color="primary" onClick={saveDate}>Registrar</Button>
                 </>)}
 
             </Form>
